cf3c0070355752601df40e33c1caa85cee727ad8
apnwong <apnwong228@gmail.com>
2019-05-26 22:47:39 +0800

    Add new file

---
+++ driving.py
@@ -0,0 +1,18 @@
+country = input('Your country: ')
+age = input('How old are you? ')
+age = int(age)
+if country == 'Taiwan':
+	if age >= 18:
+		print('You can drive')
+	else:
+		print('You cannot drive')
+elif country == 'Japan':
+	if age >= 20:
+		print('You can drive')
+	else:
+		print('You cannot drive')
+elif country == 'Amerian':
+	if age >= 16:
+		print('You can drive')
+	else:
+		print('You cannot drive')
